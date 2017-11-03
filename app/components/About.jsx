e71de141124ebb498db148dad311706536a9c1f6
andrewbalberchak <abalberchak@gmail.com>
2017-11-02 22:05:23 -0700

    More css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ $('p')
 var About = (props) => {
 	return (
 		<div>
-			<h1 className="text-center">About</h1>
+			<h1 className="text-center page-title">About</h1>
 			<p>This a weather application built with React from the React Web App Developer Course</p>
 			<p> The app uses the following tools:</p>
 			<ul>
