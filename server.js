839ffd23eac1d504ad54323f25c454904f7636cd
andrewbalberchak <abalberchak@gmail.com>
2017-11-02 23:47:24 -0700

    Added repeated pinging to server file

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
+var http = require("http");
+setInterval(function() {
+    http.get("http://sleepy-lowlands-76380.herokuapp.com/#/?_k=p8majr");
+}, 1800000); // every 5 minutes (300000)
+
 app.listen(PORT, function() {
 	console.log("Express is running on port " + PORT);
 });
